89b82ae3a406daf680c436cfab6456308096585a
lotarys <dima_advanced@mail.ru>
2024-02-19 22:37:09 +0300

    Initial commit

---
+++ src/main/java/ru/romanov/watchtogether/WatchTogetherApplication.java
@@ -0,0 +1,13 @@
+package ru.romanov.watchtogether;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WatchTogetherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WatchTogetherApplication.class, args);
+    }
+
+}
